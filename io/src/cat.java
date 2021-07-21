47fe6d4823b69902fd0733df77dc93774389b84a
Andrew Gregersen <andrew.gregersen@revature.net>
2021-07-21 13:21:21 -0700

    Limited how much text is outputed from cat

--- io/src/cat.java
+++ io/src/cat.java
@@ -1,19 +1,35 @@
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class cat {
 
     public static void main(String[] args) {
         if (args.length == 0)
-            throw new IllegalArgumentException("Usage: [path] path to file");
+            throw new IllegalArgumentException("Usage: [path]");
         try {
+
+            //Import the file into the program
             File file = new File(args[0]);
-            Scanner fileReader = new Scanner(file);
-            while (fileReader.hasNextLine()) {
-                System.out.println(fileReader.nextLine());
+            BufferedReader fileReader = new BufferedReader(new FileReader(file));
+
+
+            int counter = 1;
+            Scanner input = new Scanner(System.in);
+            //Start printing the file to the console.
+            while (fileReader.ready()) {
+                if (counter % 30 == 0) {
+                    System.out.println("----------Press Enter to Continue----------");
+//                    while (!KeyStroke.getKeyStroke('q').isOnKeyRelease()) {
+//                    }
+                    input.next();
+                }
+                System.out.println(fileReader.readLine());
+                counter++;
             }
-        } catch (FileNotFoundException ex) {
+        } catch (IOException ex) {
             System.out.println("Something went wrong " + ex.getMessage());
         }
 
