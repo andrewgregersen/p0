299d679f4c8718ffadb76b5ac0344bd267579e1b
Andrew Gregersen <andrew.gregersen@revature.net>
2021-07-21 12:16:10 -0700

    Updated ReadMe and implemented cat, grep, and the analyzer

---
+++ README.md
@@ -0,0 +1,16 @@
+# Research Utils
+
+A small collection of programs that help a user search and read documents from the command line. It also provides for
+lexical breakdown of the words and their frequency in a document.
+
+## Actors & Features
+
+- A User can:
+    - Specify which document they want to have printed out to console.
+    - Specify which search term they want to search
+    - Specify which document to analyze
+    - Access all parts of the application from one location
+
+- The Application can:
+    - Read and write to the file system
+    - Accept User input from the console
--- io/src/cat.java
+++ io/src/cat.java
@@ -1,8 +1,21 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class cat {
 
     public static void main(String[] args) {
-        if (args.length < 1)
-            throw new IllegalArgumentException("Usage: [path], [-l] number of lines default 15");
+        if (args.length == 0)
+            throw new IllegalArgumentException("Usage: [path] path to file");
+        try {
+            File file = new File(args[0]);
+            Scanner fileReader = new Scanner(file);
+            while (fileReader.hasNextLine()) {
+                System.out.println(fileReader.nextLine());
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println("Something went wrong " + ex.getMessage());
+        }
 
     }
 }
